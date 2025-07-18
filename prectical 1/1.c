4291005b9757c0d1e93c44c429f849580a069a12
Hetvi2612 <hetvidalsaniya0@gmail.com>
2025-07-18 16:24:49 +0530

    Add files via upload

---
+++ prectical 1/1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+//this program show the area and perimeter of the jogging track
+{
+    int Area;
+    int Length=70;
+    int Breadth=90;
+    int Perimeter;
+    printf("Length is 70 meter\n");
+    printf("Breadth is 90 meter\n");
+    Area=Length*Breadth;
+    printf("Area of the rectangle is: %d\n",Area);
+    Perimeter=2*Length+Breadth;
+    printf("Perimeter of the rectangle is: %d\n",Perimeter);
+    printf("This project is created by Dalsaniya Hetvi@25TCEKOF");
+    return 0;
+}
