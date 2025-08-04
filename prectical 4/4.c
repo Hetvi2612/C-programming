c53adc9fb2a58445bcf3d904759169d9da8b6ffa
Hetvi2612 <hetvidalsaniya0@gmail.com>
2025-08-04 14:23:02 +0530

    Add files via upload

---
+++ prectical 4/4.c
@@ -0,0 +1,16 @@
+#includ<stdio.h>
+#include<limits.h>
+#include<flot.h>
+void main()
+{
+    printf("The size of integer:%zu\n" sizeof(int));
+    printf("The size of character:%zu\n" sizeof(char));
+    printf("The size of integer data type is:%d\n",INT_MIN,INT_MAX);
+    printf("The size of char is: %d TO %n",CHAR_MIN,CHAR_MAX);
+    printf("The size of unsigned integer:%zu\n"sizeof(unsigned int));
+    printf("The size of unsigned int is : 0 to%u\n",UNIT_MIN,UNIT_MAX);
+    printf("The size of long int is: "sizeof(long));
+    printf("The size of long is : %ld to %ld",LONG_MIN,LONG_MAX);
+    printf("The size of ulong is:%lu\n"sizeof(unsigned long));
+    printf("The size of ulong is:  ",)
+}
