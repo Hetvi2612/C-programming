8fe6146802d0d1a2dfa53cf6942e13950f7d3c47
Hetvi2612 <hetvidalsaniya0@gmail.com>
2025-08-04 14:23:34 +0530

    Add files via upload

---
+++ prectical 6/6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+
+    float celsius,fahrenhit;
+    printf("Enter the tampratuer in celsius:\n ");
+    scanf("%f",&celsius);
+    fahrenhit=(celsius*9/5)+32;
+    printf("fahrenhit: %f ",fahrenhit);
+}
